6d2b68da0544bc0d6cb9a3c71b0ce31a0f516758
Mary-code24 <marinamar903@gmail.com>
2024-11-11 10:54:22 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [places, setPlaces] = useState(0);
+  const {id, name, description, image} = data[places];
+
+  const previousPlace = () => {
+    setPlaces((places => {
+      places --;
+      if(places < 0) {
+        return data.length-1;
+      }
+      return places;
+    }))
+  }
+
+  const nextPlace = () => {
+    setPlaces((places => {
+      places ++;
+      if (places > data.length -1) {
+        places = 0;
+      }
+      return places;
+    }))
+  }
+
+  return(
+
+    <div>
+      <div className='container'>
+      <h1>Best Islands to visit</h1>
+      </div>
+
+      <div className='container'>
+<img src={image} width='500px' alt='places' />
+      </div>
+
+      <div className='container'>
+<h2> {id} - {name}</h2>
+      </div>
+
+<div className='container'>
+<p>{ description }</p>
+</div>
+
+<div className='btn container'>
+<button onClick={previousPlace}>previous</button>
+<button onClick={nextPlace}>next</button>
+
+</div>
+
+  </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const data =[ 
+    {
+        id: 1,
+        name: 'Bali',
+        description: "Bali is the most popular of Indonesia's enchanting islands, thanks to its paradisal terrain, fascinating history and architecture, renowned wellness",
+        image: "https://img.freepik.com/free-photo/kelingking-beach-sunset-nusa-penida-island-bali-indonesia_335224-338.jpg?ga=GA1.1.2018907554.1702957705&semt=ais_hybrid"
+    },
+    {
+        id: 2,
+        name: 'Tahiti',
+        description: "The Islands of Tahiti. The Islands of Tahiti need no introduction. If we were bold, we'd leave it there and call it a day, but we might as well reiterate",
+        image: 'https://img.freepik.com/free-photo/tropical-beach-with-thatched-hut-clear-blue-water-palm-trees_9975-32967.jpg?ga=GA1.1.2018907554.1702957705&semt=ais_hybrid'
+    },
+    {
+        id: 3,
+        name: 'Aruba',
+        description: "Aruba has some great beaches and is an amazing place to go diving. The white sand beaches, culinary delights, and endless possibilities for exploration on",
+        image: 'https://img.freepik.com/free-photo/landscape-tropical-vacation-palm-summer_1203-5352.jpg?ga=GA1.1.2018907554.1702957705&semt=ais_hybrid'
+    },
+    {
+        id: 4,
+        name: 'Maldives',
+        description: "Maldives. This South Asian archipelago is often considered the home of the world's best beach thanks to its immaculate waters, white-sand beaches, and miles",
+        image: "https://images.pexels.com/photos/1483053/pexels-photo-1483053.jpeg?cs=srgb&dl=pexels-asadphoto-1483053.jpg&fm=jpg"
+    },
+    {
+        id: 5,
+        name: 'Galapagos Islands',
+        description: "The Galápagos Islands is one of the most famous tropical destinations in the world, in. The Galápagos Islands is one of the most well known tropical ",
+        image: 'https://www.travelandleisure.com/thmb/YNwVg7moCejo1S1SHGHMRHYHzwM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-galapagos-islands-08-WORLDSBESTISLANDS24-a683a5699f4f4bd8b7005edcbccb0773.jpg'
+    },
+    {
+        id: 6,
+        name: 'Palawan',
+        description: "Palawan, The Philippines. Towering limestone cliffs, bright blue waters, and sugar-sand beaches attract lots of travelers to Palawan. ",
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/13/Kayangan_Lake%2C_Coron_-_Palawan.jpg'
+    },
+
+{
+        id: 7,
+        name: 'Jamaica',
+        description: "Jamaica is a paradise for anyone looking to explore, unwind, and enjoy a mix of nature and luxury. This island is just an hour and a half away from some US",
+        image: 'https://images.unsplash.com/photo-1518950957614-73ac0a001408?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aXNsYW5kc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+        id: 8,
+        name: 'Mauritius',
+        description: "Mauritius. This vibrant volcanic island country in the Indian Ocean known for its waterfalls, mountains, beaches, and lagoons, and a helicopter flight ove",
+        image: 'https://images.unsplash.com/photo-1520769490916-8c754d52b8e0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGlzbGFuZHN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+        id: 9,
+        name: 'Santorini',
+        description: "Santorini is one of the most-visited Greek islands, and for good reason. This lively isle draws crowds, especially in summer, with its stunning sunsets",
+        image: 'https://images.unsplash.com/photo-1696519669687-44a05404eadf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNhbnRvcmluaXxlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+        id: 10,
+        name: 'Koh Samui',
+        description: "Koh Samui is renowned for its pristine beaches, turquoise waters, and lush tropical landscapes, offering the perfect setting for a relaxing getaway",
+        image: 'https://images.unsplash.com/photo-1543489822-c49534f3271f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8a29oJTIwc2FtdWl8ZW58MHx8MHx8fDA%3D'
+    },
+
+]
